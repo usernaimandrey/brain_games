21fcc3b634671cffc3f1aea369e7e45dff31dc54
usernaimandrey <anshlyapnikov@yandex.ru>
2022-12-21 01:13:38 +0300

    init commit

---
+++ lib/brain_gemes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "brain_gemes/version"
+
+module BrainGemes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/brain_gemes/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BrainGemes
+  VERSION = "0.1.0"
+end
